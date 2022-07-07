95a1868d44857f9bf43f8c6b4f1dc052d880e16a
T-Miller94 <miller.thomas1337@gmail.com>
2022-07-07 14:57:33 -0500

    add current, dev database

--- server/database/migration.sql
+++ server/database/migration.sql
@@ -2,15 +2,15 @@ DROP DATABASE IF EXISTS boomtube_db;
 CREATE DATABASE boomtube_db;
 \c
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS videos;
 DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS likes;
 DROP TABLE IF EXISTS history;
 DROP TABLE IF EXISTS favorites;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS videos;
 
 CREATE TABLE users(
-    user_id SERIAL,
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(25),
     password VARCHAR(25),
     avatar_url VARCHAR(64),
@@ -25,7 +25,25 @@ CREATE TABLE videos(
     thumbnail_url VARCHAR(64),
     descrition TEXT
 );
-CREATE TABLE users();
-CREATE TABLE users();
-CREATE TABLE users();
-CREATE TABLE users();
+
+CREATE TABLE comments (
+    comment_id SERIAL,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    video_id INTEGER REFERENCES videos ON DELETE CASCADE,
+    comment_text TEXT
+);
+
+CREATE TABLE likes(
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    video_id INTEGER REFERENCES videos ON DELETE CASCADE
+);
+
+CREATE TABLE history(
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    video_id INTEGER REFERENCES videos ON DELETE CASCADE
+);
+
+CREATE TABLE favorites(
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    video_id INTEGER REFERENCES videos ON DELETE CASCADE
+);
