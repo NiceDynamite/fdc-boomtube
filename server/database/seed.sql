0bca2367846cc3b522c9da69046b35ec21373d82
T-Miller94 <miller.thomas1337@gmail.com>
2022-07-08 15:07:13 -0500

    fixes database seed functionality; discovered process.env issues

--- server/database/seed.sql
+++ server/database/seed.sql
@@ -9,11 +9,11 @@ INSERT INTO users
     )
     VALUES 
     (
-        "Test",
-        "pass",
-        "https://cdn2.thecatapi.com/images/McgHnSYS_.jpg",
-        "Some sample text",
-        "howbout@no.com",
+        'Test',
+        'pass',
+        'https://cdn2.thecatapi.com/images/McgHnSYS_.jpg',
+        'Some sample text',
+        'howbout@no.com',
         false
     );
 
@@ -28,11 +28,11 @@ INSERT INTO users
     )
     VALUES 
     (
-        "Test",
-        "pass",
-        "https://cdn2.thecatapi.com/images/5a.gif",
-        "Some sample text",
-        "howbout@no.com",
+        'Test',
+        'pass',
+        'https://cdn2.thecatapi.com/images/5a.gif',
+        'Some sample text',
+        'howbout@no.com',
         false
     );
 
@@ -47,11 +47,11 @@ INSERT INTO users
     )
     VALUES 
     (
-        "Test",
-        "pass",
-        "https://cdn2.thecatapi.com/images/tcWReMaFB.jpg",
-        "Some sample text",
-        "howbout@no.com",
+        'Test',
+        'pass',
+        'https://cdn2.thecatapi.com/images/tcWReMaFB.jpg',
+        'Some sample text',
+        'howbout@no.com',
         false
     );
 
@@ -66,11 +66,11 @@ INSERT INTO users
     )
     VALUES 
     (
-        "Test",
-        "pass",
-        "https://cdn2.thecatapi.com/images/774.jpg",
-        "Some sample text",
-        "howbout@no.com",
+        'Test',
+        'pass',
+        'https://cdn2.thecatapi.com/images/774.jpg',
+        'Some sample text',
+        'howbout@no.com',
         false
     );
 
@@ -85,11 +85,11 @@ INSERT INTO users
     )
     VALUES 
     (
-        "Test",
-        "pass",
-        "https://cdn2.thecatapi.com/images/4qG-5FC0J.jpg",
-        "Some sample text",
-        "howbout@no.com",
+        'Test',
+        'pass',
+        'https://cdn2.thecatapi.com/images/4qG-5FC0J.jpg',
+        'Some sample text',
+        'howbout@no.com',
         false
     );
 
@@ -104,10 +104,10 @@ INSERT INTO users
     )
     VALUES 
     (
-        "Test",
-        "pass",
-        "https://cdn2.thecatapi.com/images/Ua8Kr2RuM.jpg",
-        "Some sample text",
-        "howbout@no.com",
+        'Test',
+        'pass',
+        'https://cdn2.thecatapi.com/images/Ua8Kr2RuM.jpg',
+        'Some sample text',
+        'howbout@no.com',
         false
     );
--- server/server.js
+++ server/server.js
@@ -1,18 +1,35 @@
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express')
-const pool = require('./database/connection.js')
+// const pool = require('./database/connection.js')
+const {Pool} = require('pg')
 const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 5000
-const DATABASE_URL = process.env.DATABASE_URL
+// const DATABASE_URL = process.env.DATABASE_URL
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 
+const pool = new Pool({
+    host: 'localhost',
+    user: 'alex',
+    password: '1313',
+    database: 'boomtube_db'
 
+})
+
+app.get('/users', async (req, res)=> {
+    try {
+        let data = await pool.query(`SELECT * FROM users`)
+        res.json(data.rows)
+    } catch (error) {
+        console.log(error.message)
+        res.send(error.message)
+    }
+})
 
 app.listen(PORT, ()=>{
-    console.log(`Connecting to: ${DATABASE_URL}`)
-    console.log(`Listing on ${PORT}`)
+    // console.log(`Connecting to: ${DATABASE_URL}`)
+    console.log(`Listening on ${PORT}`)
 })
