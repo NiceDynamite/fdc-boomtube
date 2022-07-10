8f89ae04b76dd175ab9a455cef11f327e5345e8c
Aaron <aaron@Aarons-Air.lan>
2022-07-10 19:53:12 -0400

    added layout for Signup Page

---
+++ src/Pages/SignupPage.js
@@ -0,0 +1,28 @@
+import SignupForm from "../Components/SignupForm"
+import { Paper } from '@mui/material'
+
+export default function SignupPage() {
+    return (
+        <div className="signup">
+            <div className="signup-container">
+                <Paper
+                    elevation={6}
+                    sx={{
+                        minWidth: '400px',
+                        minHeight: '400px',
+                        borderRadius: '10px'
+                    }}
+                >
+                    <div className="signup-title">
+                        <h1 className="welcome-title">Welcome Friend!</h1>
+                        <p className="welcome-message">Signup below to register</p>
+                    </div>
+                    <div className="form-container">
+                        <SignupForm />
+                    </div>
+                </Paper>
+            </div>
+        </div>
+    )
+}
+
