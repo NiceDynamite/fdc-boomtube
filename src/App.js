663eea2535cf786be4075c67f93db9370d15748f
Aaron <aaron@Aarons-Air.lan>
2022-07-10 19:50:01 -0400

    added route for signup page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom"
 import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
 
 export default function App() {
     return (
         <>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
             </Routes>
         </>
     )
