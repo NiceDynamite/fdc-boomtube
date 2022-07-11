cb9f1a8b6f22b3f84bde30d1775eb4c13535b6e4
georgehniii <georgehniii@gmail.com>
2022-07-10 17:18:05 -0700

    Working branch pausing to merge from master

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,20 +1,24 @@
 import LoginForm from "../Components/LoginForm";
 import logo from "./boomtubelogo.png"
 import './LoginPage.css'
+import Header from '../Components/header/HeaderMain.js';
 
 export default function LoginPage() {
     return (
-        <div className="login">
-            <img className="login-logo" alt="BoomTube Logo" src={logo} height="300px" width="275px" />
-            <div className="login-container">
-                <div className="login-title">
-                    <h1 className="welcome-title">Welcome Back</h1>
-                    <p className="welcome-message">Sign back in to continue</p>
-                </div>
-                <div className="form-container">
-                    <LoginForm />
+        <>
+            <Header/>
+            <div className="login">
+                <img className="login-logo" alt="BoomTube Logo" src={logo} height="300px" width="275px" />
+                <div className="login-container">
+                    <div className="login-title">
+                        <h1 className="welcome-title">Welcome Back</h1>
+                        <p className="welcome-message">Sign back in to continue</p>
+                    </div>
+                    <div className="form-container">
+                        <LoginForm />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
