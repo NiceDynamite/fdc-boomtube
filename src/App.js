a2f694fd2b67d1c8289997ac3486fd9df0a17443
Aaron <aaron@Aarons-Air.lan>
2022-07-12 15:36:08 -0400

    added auth provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom"
+import { AuthProvider } from "./contexts/AuthContext";
 import LoginPage from "./Pages/LoginPage";
 import SignupPage from "./Pages/SignupPage";
 
 export default function App() {
     return (
         <>
-            <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignupPage />} />
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignupPage />} />
+                </Routes>
+
+            </AuthProvider>
         </>
     )
 }
