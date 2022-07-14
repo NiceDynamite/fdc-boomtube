2c76d07628318ed37568860fa8421a505111cb7e
Aaron <aaron@Aarons-Air.lan>
2022-07-13 21:32:05 -0400

    removed auth provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom"
-import { AuthProvider } from "./contexts/AuthContext";
 import LoginPage from "./Pages/LoginPage";
 import SignupPage from "./Pages/SignupPage";
 
 export default function App() {
     return (
         <>
-            <AuthProvider>
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<SignupPage />} />
-                </Routes>
-
-            </AuthProvider>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+            </Routes>
         </>
     )
 }
